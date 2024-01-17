3431ba66f66452878cd29135cfa2279c15347fb3
andresito236 <aeparedes236@gmail.com>
2024-01-16 22:11:57 -0600

    Actual challenge solution

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
-import 'package:favorite_places_7/screens/favorite_places.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_fonts/google_fonts.dart';
+
+import 'package:favorite_places_7/screens/places.dart';
 
 final colorScheme = ColorScheme.fromSeed(
   brightness: Brightness.dark,
@@ -27,22 +28,20 @@ final theme = ThemeData().copyWith(
 );
 
 void main() {
-  runApp(const ProviderScope(
-    child:  MyApp(),
-    )      
+  runApp(
+    const ProviderScope(child: MyApp()),
   );
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Great Places',
       theme: theme,
-      debugShowCheckedModeBanner: false,
-      home: const FavoritePlacesList(),
+      home: const PlacesScreen(),
     );
   }
 }
